b411806438784ea6358b90b19ac74bebf239ab24
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-12-05 17:08:08 +0100

    video2

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,13 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+final class MainController extends AbstractController
 {
-    #[Route('/brouette')]
-    public function index(): Response {
-        return new Response('Hello');
+    #[Route('/', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'prenom' => 'Gwld',
+        ]);
+    }
+
+    #[Route('/mentions-legales', name: 'app_mentions')]
+    public function mentions(): Response
+    {
+        return new Response('Mentions légales');
     }
 }
